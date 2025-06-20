b7ec36011afb75cf9336c538e64cade3999bbd77
Dilan Bandara <dilan.harshana.739@gmail.com>
2025-06-21 00:37:04 +1200

    Home Page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import MovieCard from './components/MovieCard';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
-      <MovieCard movie={{title:"Film", release_date:"2024"}}/>
+      <Home />
     </> 
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,44 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+function Home() {
+
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const movies = [
+        {id: 1, title: "Inception", release_date: "2010"},
+        {id: 2, title: "Interstellar", release_date: "2014"},
+        {id: 3, title: "The Dark Knight", release_date: "2008"}
+    ]
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(`Searching for: ${searchQuery}`);
+        setSearchQuery("");
+    }
+
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input 
+                    type="text" 
+                    placeholder="search for movies.." 
+                    className="search-input" 
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-button">Search</button>
+            </form>
+
+            <div className="movies-grid">
+                {movies.map((movie) => 
+                    (
+                        <MovieCard movie={movie} key={movie.id}/> 
+                    )
+                )}    
+            </div>
+        </div>
+    );
+}
+
+export default Home;
