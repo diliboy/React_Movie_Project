a934e98a3faf4a03b3767c70b29a8caa375ccaf4
Dilan Bandara <dilan.harshana.739@gmail.com>
2025-06-21 15:40:24 +1200

    Added NavBar and Favorites page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css'
-import MovieCard from './components/MovieCard';
 import Home from './pages/Home';
+import Favorites from './pages/Favorites';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
-    <>
-      <Home />
-    </> 
+    <div>
+      <Navbar />
+    
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/favorites" element={<Favorites/>}/>
+        </Routes>
+      </main>
+    </div>
   );
 }
 
